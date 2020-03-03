2fcdccec719f4218128dce27d2ab9212ef97b880
dhriffel <31215851+dhriffel@users.noreply.github.com>
2020-03-03 09:51:32 -0600

    Add files via upload

--- src/test/java/me/dhriffel/wirelessservice/databaseTest.java
+++ src/test/java/me/dhriffel/wirelessservice/databaseTest.java
@@ -1,8 +1,10 @@
 package me.dhriffel.wirelessservice;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"json:target/cucumber.json"})
 public class databaseTest {
 }
